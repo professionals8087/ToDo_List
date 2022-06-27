1340d9ece87233f8a6738ceef6063fc69429c4b4
fatemeh <f.robatjazi.1380@gmail.com>
2022-06-27 11:04:04 +0430

    Add Css And Js, Install Packages And Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,26 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
+
+
+    mix.styles([
+        'resources/css/bootstrap.css',
+        'resources/css/owl.carousel.css',
+        'resources/css/owl.theme.default.css',
+    ], 'public/css/all.css');
+    
+    
+    mix.scripts([
+        'resources/js/jquery.js',
+        'resources/js/bootstrap.js',
+        'resources/js/owl.carousel.min.js',
+        'resources/js/index.js'
+    ], 'public/js/all.js');
